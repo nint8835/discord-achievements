66b26ad953799e585dcd4dacb07779f2b403e8ba
Riley Flynn <riley@rileyflynn.me>
2023-05-06 15:40:05 -0230

    Prettier tailwind plugin

--- pkg/app/frontend/src/App.tsx
+++ pkg/app/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-    return <div className="text-violet-400">Hello world</div>;
+    return <div className="bg-black text-violet-400">Hello world</div>;
 }
 
 export default App;
