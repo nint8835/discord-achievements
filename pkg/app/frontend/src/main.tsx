2042651a858a23549355846a072e6b68d08be3e0
Riley Flynn <riley@rileyflynn.me>
2023-05-06 21:43:26 -0230

    Init react query

--- pkg/app/frontend/src/main.tsx
+++ pkg/app/frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import '@fontsource/ibm-plex-mono';
 import '@fontsource/ibm-plex-sans';
 import '@fontsource/ibm-plex-sans/700.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import Root from './routes/Root';
 import RouterError from './routes/RouterError';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
     {
@@ -18,6 +22,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </React.StrictMode>,
 );
