6c89248fa724e9fc1814eeb4d142e682f1ab887f
Riley Flynn <riley@rileyflynn.me>
2023-05-07 14:54:14 -0230

    Error handling on owned bundles

--- pkg/app/frontend/src/queries/achievement_bundles.ts
+++ pkg/app/frontend/src/queries/achievement_bundles.ts
@@ -11,6 +11,11 @@ type AchievementBundle = {
 
 export async function getOwnedBundles(): Promise<AchievementBundle> {
     const response = await fetch('/achievement-bundles/owned');
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
     return await response.json();
 }
 
