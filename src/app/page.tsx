46d7678cc31a4fd09a8abea10b36a3cadf4c1c4c
Riley Flynn <riley@rileyflynn.me>
2023-09-28 20:52:22 -0230

    No more overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <div className="flex h-screen w-screen items-center justify-center">Home</div>;
+    return <div className="flex w-screen flex-1 items-center justify-center">Home</div>;
 }
