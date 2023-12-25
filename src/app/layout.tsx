690bdd0d33497808c5e25e0b7eeae92a35627f80
Riley Flynn <riley@rileyflynn.me>
2023-12-25 15:05:15 -0330

    Re-add inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
+import { Inter } from 'next/font/google';
+
 import './globals.css';
+
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className={inter.variable}>
             <body>{children}</body>
         </html>
     );
