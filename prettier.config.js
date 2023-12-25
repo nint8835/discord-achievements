27ea0eaebdc26138fc7fb0418a47ed6aea50f91b
Riley Flynn <riley@rileyflynn.me>
2023-12-25 14:46:49 -0330

    Automatic import sorting

--- prettier.config.js
+++ prettier.config.js
@@ -4,5 +4,8 @@ module.exports = {
     singleQuote: true,
     printWidth: 120,
     tabWidth: 4,
-    plugins: ['prettier-plugin-tailwindcss'],
+    plugins: ['prettier-plugin-tailwindcss', '@trivago/prettier-plugin-sort-imports'],
+    importOrder: ['^@/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
 };
