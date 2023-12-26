bd81e7fb54d1141af05485668179228c1ab54054
Riley Flynn <riley@rileyflynn.me>
2023-12-26 10:36:12 -0330

    More schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,18 +8,31 @@ export const users = sqliteTable('users', {
 
 export const usersRelations = relations(users, ({ many }) => ({
     earnedAchievements: many(earnedAchievements),
+    ownedAchievements: many(achievements),
+    ownedAchievementBundles: many(achievementBundles),
 }));
 
 export const achievements = sqliteTable('achievements', {
     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-    unique: integer('unique', { mode: 'boolean' }).default(false),
-    // TODO: Who / what owns an achievement?
-    // TOOD: Associate achievements to either a guild or an integration
-    // TODO: Actual display fields (name, description, maybe icon, etc.)
+    ownerId: text('owner_id')
+        .notNull()
+        .references(() => users.id),
+
+    name: text('name').notNull(),
+    description: text('description'),
+    unique: integer('unique', { mode: 'boolean' }).notNull().default(false),
+    // TODO: Icon?
+
+    guildId: text('guild_id').references(() => guilds.id),
+    bundleId: integer('bundle_id', { mode: 'number' }).references(() => achievementBundles.id),
+    // TODO: Integrations
 });
 
-export const achievementsRelations = relations(achievements, ({ many }) => ({
+export const achievementsRelations = relations(achievements, ({ one, many }) => ({
     usersEarned: many(earnedAchievements),
+    owner: one(users, { fields: [achievements.ownerId], references: [users.id] }),
+    guild: one(guilds, { fields: [achievements.guildId], references: [guilds.id] }),
+    bundle: one(achievementBundles, { fields: [achievements.bundleId], references: [achievementBundles.id] }),
 }));
 
 export const earnedAchievements = sqliteTable(
@@ -39,4 +52,32 @@ export const earnedAchievements = sqliteTable(
 export const earnedAchievementsRelations = relations(earnedAchievements, ({ one }) => ({
     achievement: one(achievements, { fields: [earnedAchievements.achievementId], references: [achievements.id] }),
     user: one(users, { fields: [earnedAchievements.userId], references: [users.id] }),
+}));
+
+export const achievementBundles = sqliteTable('achievement_bundles', {
+    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+
+    name: text('name').notNull(),
+    description: text('description'),
+
+    ownerId: text('owner_id')
+        .notNull()
+        .references(() => users.id),
+    guildId: text('guild_id').references(() => guilds.id),
+});
+
+export const achievementBundlesRelations = relations(achievementBundles, ({ one, many }) => ({
+    achievements: many(achievements),
+    owner: one(users, { fields: [achievementBundles.ownerId], references: [users.id] }),
+    guild: one(guilds, { fields: [achievementBundles.guildId], references: [guilds.id] }),
+}));
+
+export const guilds = sqliteTable('guilds', {
+    id: text('id').primaryKey(),
+    name: text('name').notNull(),
+});
+
+export const guildsRelations = relations(guilds, ({ one, many }) => ({
+    achievements: many(achievements),
+    achievementBundles: many(achievementBundles),
 }));
